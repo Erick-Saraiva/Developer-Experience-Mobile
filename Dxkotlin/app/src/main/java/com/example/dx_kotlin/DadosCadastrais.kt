78dc4015583e536c7dda11f47abad8e61aa87092
raulpetersonsantos <raul.santos@bandtec.com.br>
2023-04-12 20:51:24 -0300

    tela de cadastro empresa finalizado

---
+++ Dxkotlin/app/src/main/java/com/example/dx_kotlin/DadosCadastrais.kt
@@ -0,0 +1,11 @@
+package com.example.dx_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DadosCadastrais : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dados_cadastrais)
+    }
+}
